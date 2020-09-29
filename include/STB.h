7ed1e6f3b63bf51caed9773e766bebaca4401bb7
Milica <trifunoviceva97@gmail.com>
2020-09-29 11:05:15 +0200

    GetServiceDescription and add Action and Argument structs

--- include/STB.h
+++ include/STB.h
@@ -1,51 +1,66 @@
 #include <iostream> 
 #include <string>
 #include <vector>
+#include <algorithm> 
+#include "MySocket.h"
 
 class STB
 {
     public:
-        STB(std::string fname, std::string uuid, std::string address, std::string port, std::string xmlLocation, std::string nt);
-        STB(std::string uuid, std::string address, std::string port, std::string xmlLocation, std::string nt);
+        STB(std::string fname, std::string uuid, std::string address, std::string port, std::string xmlLocation);
+        STB(std::string uuid, std::string address, std::string port, std::string xmlLocation);
         
         void AddService(std::string type, std::string id, std::string controlURL, std::string eventURL, std::string descriptionURL);
         void ShowMyServices();
+        void SearchServiceDescription(std::string serviceName);
 
         std::string GetFriendlyName() const { return this->friendlyName; }
         std::string GetUUID() const { return this->uuid; }
         std::string GetAddress() const { return this->address; }
         std::string GetPort() const { return this->port; }
-        std::string GetXMLLocation() const { return this->configXMLLocation; }
-        std::string GetNt() const { return this->nt; }      
+        std::string GetXMLLocation() const { return this->configXMLLocation; }    
 
         void SetFriendlyName(std::string fname) { this-> friendlyName = fname; }
 
-    private: 
+    private:
+        enum class DirectionType { IN, OUT };
+        enum class ArgumentType { INT, FLOAT, CHAR, STRING, BOOLEAN };
+
+        struct Argument
+        {
+            std::string name;
+            DirectionType directionType;
+            std::string relatedStateVariable;
+            ArgumentType type;
+
+            Argument(std::string name, DirectionType directionType, std::string relatedStateVariable, ArgumentType type);
+        };
+
+        struct Action
+        {
+            std::string name;
+            std::vector<Argument> InputParameter;
+            std::vector<Argument> OutputParameter;
+
+            void AddArgument(std::string name, DirectionType directionType, std::string relatedStateVariable, ArgumentType type);
+        };
+
         struct Service{
             std::string type;
             std::string id;
             std::string descriptionURL;
             std::string controlURL;
             std::string eventURL;
+            std::vector<Action> actions;
 
-            std::string GetNameOfService() const
-            {
-                ushort nameBegin = this->type.find(":service:") + 9;
-                ushort nameEnd = this->type.find(':', nameBegin);
-                return this->type.substr(nameBegin, nameEnd - nameBegin);
-            }
+            std::string GetNameOfService() const;
+            std::string GetVersionOfService() const;
+            std::string GetServiceId() const;
+ 
+            void AddAction(std::string name);
+            bool GetServiceDescription(std:: string STBAddress, std::string STBPort);
 
-            std::string GetVersionOfService() const
-            {
-                ushort versionBegin = this->type.rfind(':') + 1;
-                return this->type.substr(versionBegin, this->type.length() - versionBegin);
-            }
-
-            std::string GetServiceId() const
-            {
-                ushort idBegin = this->id.find(":serviceId:") + 11;
-                return this->id.substr(idBegin, this->id.length() - idBegin);
-            }
+            void FillActionList(std::string XMLresponse);
         };
         
         std::string friendlyName;
@@ -53,6 +68,5 @@ class STB
         std::string address;
         std::string port;
         std::string configXMLLocation;
-        std::string nt;
         std::vector<Service> services;
 };
--- src/STB.cpp
+++ src/STB.cpp
@@ -1,13 +1,15 @@
 #include <STB.h>
 
-STB::STB(std::string fname, std::string uuid, std::string address, std::string port, std::string xmlLocation, std::string nt)
-    :STB(uuid, address, port, xmlLocation, nt)
+#define BUFF_SIZE 1024
+
+STB::STB(std::string fname, std::string uuid, std::string address, std::string port, std::string xmlLocation)
+    :STB(uuid, address, port, xmlLocation)
 {
     this->friendlyName = fname;
 }
 
-STB::STB(std::string uuid, std::string address, std::string port, std::string xmlLocation, std::string nt)
-    :uuid(uuid), address(address), port(port), configXMLLocation(xmlLocation), nt(nt)
+STB::STB(std::string uuid, std::string address, std::string port, std::string xmlLocation)
+    :uuid(uuid), address(address), port(port), configXMLLocation(xmlLocation)
 {
 }
 
@@ -21,4 +23,88 @@ void STB::ShowMyServices()
     std::cout << "STB: " << this->friendlyName << std::endl;
     for(auto const &service : this->services)
         std::cout << service.GetNameOfService() << " : " << service.GetVersionOfService() << std::endl;
+}
+
+void STB::SearchServiceDescription(std::string serviceName)
+{
+    auto service = std::find_if(this->services.begin(), this->services.end(), [&serviceName](const Service& s) { return !s.GetNameOfService().compare(serviceName); });
+    if(service != this->services.end())
+        service->GetServiceDescription(this->GetAddress(), this->GetPort());
+}
+
+STB::Argument::Argument(std::string name, DirectionType directionType, std::string relatedStateVariable, ArgumentType type)
+    :name(name), directionType(directionType), relatedStateVariable(relatedStateVariable), type(type)
+{
+}
+
+void STB::Action::AddArgument(std::string name, DirectionType directionType, std::string relatedStateVariable, ArgumentType type)
+{
+    Argument argument(name, directionType, relatedStateVariable, type);
+    if(directionType == DirectionType::IN)
+        this->InputParameter.push_back(argument);
+    else
+        this->OutputParameter.push_back(argument);
+}
+
+std::string STB::Service::GetNameOfService() const
+{
+    ushort nameBegin = this->type.find(":service:") + 9;
+    ushort nameEnd = this->type.find(':', nameBegin);
+    return this->type.substr(nameBegin, nameEnd - nameBegin);
+}
+
+std::string STB::Service::GetVersionOfService() const
+{
+    ushort versionBegin = this->type.rfind(':') + 1;
+    return this->type.substr(versionBegin, this->type.length() - versionBegin);
+}
+
+std::string STB::Service::GetServiceId() const
+{
+    ushort idBegin = this->id.find(":serviceId:") + 11;
+    return this->id.substr(idBegin, this->id.length() - idBegin);
+}
+
+bool STB::Service::GetServiceDescription(std:: string STBAddress, std::string STBPort)
+{
+    std::string msg = "GET " + this->descriptionURL + " HTTP/1.1\r\n"
+                        "Connection: close\r\n"
+                        "Host: " + STBAddress + ":" + STBPort + "\r\n"
+                        "\r\n";
+        
+    MySocket sock(AF_INET, SOCK_STREAM);
+    if(!sock.CreateSocket())
+        return false;
+    
+    if(!sock.Connect(AF_INET, std::stoi(STBPort), inet_addr(STBAddress.c_str())))
+        return false;
+
+    sock.Send(msg.c_str(), msg.length(), 0);
+
+    std::string XMLresponse = "";
+    char recvBuf[BUFF_SIZE];
+    memset(recvBuf, 0, BUFF_SIZE);
+
+    while(sock.Recieve(recvBuf, BUFF_SIZE, 0) != 0)
+    {
+        if(!XMLresponse.empty())
+            XMLresponse.pop_back();
+        XMLresponse += recvBuf;
+        memset(recvBuf, 0, BUFF_SIZE);
+    }
+    
+    if(!XMLresponse.empty())
+    {
+        //std::cout << XMLresponse << std::endl;
+        FillActionList(XMLresponse);
+
+        return true;
+    }
+    else
+        return false;
+}
+
+void STB::Service::FillActionList(std::string XMLresponse)
+{
+    
 }

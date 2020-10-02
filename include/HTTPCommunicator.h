d42fd02441a233105eb286ad1fb1cf15801acd60
Nikola <trajko97@gmail.com>
2020-10-02 14:09:54 +0200

    Added Post method in HTTPCommunicator

--- include/HTTPCommunicator.h
+++ include/HTTPCommunicator.h
@@ -3,11 +3,14 @@
 #include <string>
 #include "MySocket.h"
 
+#define USER_AGENT "Linux/20.04 UPnP/2.0 upnpSearcher/1.0"
+
 class HTTPCommunicator
 {
     private:
         HTTPCommunicator() {}
-
+        static std::string PostBodyFromResponse(std::string PostResponse);
     public:
         static std::string GetXMLDescription(std::string XMLUrl, std::string address, std::string port);
+        static std::string PostExecuteAction(std::string controlURL, std::string address, std::string port, std::string soapAction, std::string body);
 };
--- src/HTTPCommunicator.cpp
+++ src/HTTPCommunicator.cpp
@@ -1,4 +1,5 @@
 #include "HTTPCommunicator.h"
+#include <iostream>
 
 #define BUFF_SIZE 1024
 
@@ -31,4 +32,54 @@ std::string HTTPCommunicator::GetXMLDescription(std::string XMLUrl, std::string 
     }
     
     return XMLresponse;  
+}
+
+std::string HTTPCommunicator::PostExecuteAction(std::string controlURL, std::string address, std::string port, std::string soapAction, std::string body)
+{
+    std::string msg = "POST " + controlURL + " HTTP/1.1\r\n"
+                        "HOST: " + address + ":" + port + "\r\n"
+                        "CONTENT-LENGTH: " + std::to_string(body.length()) + "\r\n"
+                        "CONTENT-TYPE: text/xml; charset=\"utf-8\"\r\n"
+                        "USER-AGENT: " + USER_AGENT + "\r\n"
+                        "SOAPACTION: \"" + soapAction + "\"\r\n"
+                        "\r\n"
+                        + body;
+        
+    MySocket sock(AF_INET, SOCK_STREAM);
+    if(!sock.CreateSocket())
+        return "";
+    
+    if(!sock.Connect(AF_INET, std::stoi(port), inet_addr(address.c_str())))
+        return "";
+
+    sock.Send(msg.c_str(), msg.length(), 0);
+
+    std::string PostResponse = "";
+    char recvBuf[BUFF_SIZE];
+    memset(recvBuf, 0, BUFF_SIZE);
+
+    while(sock.Recieve(recvBuf, BUFF_SIZE - 1, 0) != 0)
+    {
+        recvBuf[BUFF_SIZE - 1] = '\0';
+
+        PostResponse += recvBuf;
+        memset(recvBuf, 0, BUFF_SIZE);
+    }
+    
+    size_t pos = PostResponse.find(" 200 OK");
+
+    if(pos == std::string::npos)
+    {
+        size_t endLinePos = PostResponse.find("\r\n");
+        std::cout << PostResponse.substr(0, endLinePos) << '\n';
+    }
+
+    return PostBodyFromResponse(PostResponse);
+}
+
+std::string HTTPCommunicator::PostBodyFromResponse(std::string PostResponse)
+{
+    size_t soapBegin = PostResponse.find('<');
+    
+    return PostResponse.substr(soapBegin, PostResponse.length() - soapBegin);
 }
